040bd80c6f43d592ff13440a2faa7eb6c98f32d6
Andrew stecenko <andrew1stecenko@gmail.com>
2025-12-28 13:07:26 +0100

    Add initial HTML structure and main JavaScript functionality

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+const result = 5 + 5 + "5";
+console.log(typeof result);
+
+const email = "andrew1stecenko@gmail.com";
+console.log(email.includes("@"));
+
+const my = "my";
+const anyName = "name";
+const is = "is";
+const fullName = `${my} ${anyName} ${is} anredw`;
+console.log(fullName);
+
+const userName = "arndew";
+const payment = 300;
+console.log(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
